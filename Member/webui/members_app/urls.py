1e600ca9ac3026fe3d1e6854243e6f38897c5f1e
逍遥剑仙 <17145309+hans5567@user.noreply.gitee.com>
2026-06-11 12:26:41 +0800

    加入可视化编辑能力

--- Member/webui/members_app/urls.py
+++ Member/webui/members_app/urls.py
@@ -5,4 +5,8 @@ app_name = 'members_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/members/', views.api_members, name='api_members'),
+    path('api/members/add/', views.api_add_member, name='api_add_member'),
+    path('api/members/<str:gaijin_id>/update/', views.api_update_member, name='api_update_member'),
+    path('api/members/<str:gaijin_id>/delete/', views.api_delete_member, name='api_delete_member'),
 ]
--- Member/webui/webui/urls.py
+++ Member/webui/webui/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('members_app.urls')),
+    path('', include('members_app.urls')),   # 将根路径指向 members_app
 ]
